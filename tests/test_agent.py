e9dff722bb01a8ab8f83f241de13636cabb86b10
cwher <cwher@outlook.com>
2022-06-16 15:58:55 +0800

    test: add agent test

---
+++ tests/test_agent.py
@@ -0,0 +1,26 @@
+import logging
+
+import torch
+from icecream import ic
+
+from agent.d3qn import D3QN
+from train_utils import ReplayBuffer, playGame
+
+if __name__ == "__main__":
+    ic.disable()
+    logging.basicConfig(level=logging.DEBUG)
+
+    agent = D3QN()
+    agent.setDevice(torch.device("cuda:0"))
+    agent.save(version="test")
+    replay_buffer = ReplayBuffer()
+
+    # stage 1
+    episode_data, reward = playGame(agent, 19937)
+    replay_buffer.add(episode_data)
+    replay_buffer.save("test")
+
+    # stage 2
+    # replay_buffer.load("./dataset/data_test.pkl")
+    # data_batch = replay_buffer.sample()
+    # loss = agent.trainStep(data_batch)
