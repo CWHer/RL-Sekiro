d4ae1b1e934f07d2a4063f4fa652cf9ac40e0f09
cwher <cwher@outlook.com>
2022-06-18 23:10:12 +0800

    perf: use faster keyboard

--- env/actions.py
+++ env/actions.py
@@ -1,10 +1,10 @@
 import logging
 import time
 
-import pydirectinput
 from utils import timeLog
 
-from .env_config import AGENT_KEYMAP, ENV_KEYMAP
+from .env_config import AGENT_KEYMAP, ENV_KEYMAP, PRESS_RELEASE_DELAY
+from .keyboard import PressKey, ReleaseKey
 
 
 class Actor():
@@ -20,7 +20,10 @@ class Actor():
             logging.critical("invalid agent action")
             raise RuntimeError()
 
-        pydirectinput.press(self.agent_keymap[key])
+        key_code = self.agent_keymap[key]
+        PressKey(key_code)
+        time.sleep(PRESS_RELEASE_DELAY)
+        ReleaseKey(key_code)
         logging.info(f"action: {key}")
 
         time.sleep(action_delay)
@@ -32,7 +35,10 @@ class Actor():
             logging.critical("invalid env action")
             raise RuntimeError()
 
-        pydirectinput.press(self.env_keymap[key])
+        key_code = self.env_keymap[key]
+        PressKey(key_code)
+        time.sleep(PRESS_RELEASE_DELAY)
+        ReleaseKey(key_code)
         logging.debug(f"env: {key}")
 
         time.sleep(action_delay)
