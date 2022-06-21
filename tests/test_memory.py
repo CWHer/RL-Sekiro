d338a155ac7a2609ae485b43367d1413f0d3b6a6
cwher <cwher@outlook.com>
2022-06-22 00:41:25 +0800

    test: upd env tests

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -1,6 +1,6 @@
 import time
 
-from env.env_config import ACTION_DELAY, AGENT_DEAD_DELAY, MAP_CENTER
+from env.env_config import AGENT_DEAD_DELAY, MAP_CENTER
 from env.memory import Memory
 
 
@@ -9,13 +9,16 @@ if __name__ == "__main__":
     time.sleep(10)
 
     for i in range(200):
-        agent_hp, agent_ep = memory.getStatus()
+        agent_hp, agent_ep, boss_hp = memory.getStatus()
         lock_state = memory.lockBoss()
-        print(f"HP {agent_hp:<.2f}, EP {agent_ep:<.2f}")
+        print(f"{agent_hp:<.2f}, {agent_ep:<.2f}, {boss_hp:<.2f}")
         print(f"Lock State: {lock_state}")
+        if i % 50 == 0:
+            memory.resetEndurance()
+            memory.reviveBoss()
         if agent_hp == 0:
             time.sleep(AGENT_DEAD_DELAY)
             memory.transportAgent(MAP_CENTER)
             lock_state = memory.lockBoss()
-            memory.reviveAgent()
-        time.sleep(ACTION_DELAY)
+            memory.reviveAgent(need_delay=False)
+        time.sleep(0.5)
