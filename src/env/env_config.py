19694ffed4f0886ebdbb52029803c3719ca11cae
cwher <cwher@outlook.com>
2022-06-22 13:04:21 +0800

    feat: use latest env

--- src/env/env_config.py
+++ src/env/env_config.py
@@ -4,8 +4,22 @@ GAME_NAME = "Sekiro"
 # HACK: change accordingly
 PRESS_RELEASE_DELAY = 0.02
 
-STEP_DELAY = 0.15
-ACTION_DELAY = 0.25
+# NOTE: it takes time for action to take effect,
+#   STEP_DELAY ensures that the action matches its reward
+STEP_DELAY = {
+    "attack": 0.7,
+    "defense": 0.25,
+    "dodge": 0.5,
+    "jump": 0.6,
+}
+# NOTE: actions that are too frequent are ignored,
+#   ACTION DELAY represents the time between successive actions
+ACTION_DELAY = {
+    "attack": 0.6,
+    "defense": 0.4,
+    "dodge": 0.6,
+    "jump": 0.8,
+}
 
 AGENT_DEAD_DELAY = 10
 ROTATION_DELAY = 1
@@ -32,8 +46,11 @@ MIN_HELPER_LEN = 13
 # <------
 
 # ------> agent attributes
-MAX_HP = 800
-MAX_EP = 300
+MAX_AGENT_HP = 800
+MAX_AGENT_EP = 300
+
+MAX_BOSS_HP = 9887
+MAX_BOSS_EP = 4083
 
 MAP_CENTER = (-110.252, 54.077, 239.538)
 # <------
@@ -46,5 +63,5 @@ SCREEN_ANCHOR = (1, -721, -1, -1)
 FOCUS_ANCHOR = (392, 108, 892, 608)
 FOCUS_SIZE = (128, 128)
 
-BOSS_HP_ANCHOR = (75, 62, 348, 71)
+# BOSS_HP_ANCHOR = (75, 62, 348, 71)
 # <------
