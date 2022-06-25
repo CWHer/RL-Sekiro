740a361a26994a8d69792c0114a701f7a4a0df50
cwher <cwher@outlook.com>
2022-06-25 22:43:02 +0800

    fix: modify reward

--- env/sekiro_env.py
+++ env/sekiro_env.py
@@ -37,19 +37,11 @@ class SekiroEnv():
         # TODO: refine reward
         rewards = np.array(
             [agent_hp - self.last_agent_hp,
-             min(0, agent_ep - self.last_agent_ep),
-             max(0, self.last_boss_hp - boss_hp)])
-        weights = np.array([10, 10, 10])  # TODO
+             self.last_boss_hp - boss_hp
+                if boss_hp <= self.last_boss_hp
+                else self.last_boss_hp + 1 - boss_hp])
+        weights = np.array([10, 200])
         reward = weights.dot(rewards).item()
-
-        reward = -10 if agent_hp == 0 else reward
-
-        # NOTE: stage rewards
-        stages = [0.2, 0.4, 0.6, 0.8]
-        rewards = [40, 30, 20, 10]
-        current, last = np.digitize([boss_hp, self.last_boss_hp], stages)
-        reward += sum(rewards[current:last]) if current <= last \
-            else sum(rewards[current:] + rewards[:last]) + 50
 
         return reward
 
